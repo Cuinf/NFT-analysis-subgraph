0ad4071aef338e23e3b91b082197f23d31731767
Cui Qin <qincuidhu@gmail.com>
2021-05-23 21:04:17 +0200

    use a common mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,9 +1,9 @@
-import { Transfer } from '../generated/Polkamon/Polkamon'
+import { Transfer, ERC721 } from '../generated/Polkamon/ERC721'
 import { NftOwner, NftBalance } from '../generated/schema'
 import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
-    let id = event.transaction.hash.toHex()
+    let id = event.address.toHex() + '#' + event.params.tokenId.toHex();
     let nftOwner = NftOwner.load(id)
     if (nftOwner == null) {
         nftOwner = new NftOwner(id)
@@ -13,21 +13,28 @@ export function handleTransfer(event: Transfer): void {
     nftOwner.contract = event.address
     nftOwner.save()
 
-    let previousOwner = event.params.from.toHex()
-    let nftBalance = NftBalance.load(previousOwner)
-    if (nftBalance != null) {
-        if (nftBalance.amount > BigInt.fromI32(0)) {
-            nftBalance.amount = nftBalance.amount - BigInt.fromI32(1)
-        }
-        nftBalance.save()
-    }    
+    //update the amount ot the token 
+    let contract = ERC721.bind(event.address)
+    let nftBalance = new NftBalance(event.params.to.toHex())
+    nftBalance.amount = contract.balanceOf(event.params.to)
+    nftBalance.totalSupply = contract.totalSupply()
+    nftBalance.save()
 
-    let newOwner = event.params.to.toHex()
-    let newNFTBalance = NftBalance.load(newOwner)
-    if (newNFTBalance == null) {
-        newNFTBalance = new NftBalance(newOwner)
-        newNFTBalance.amount = BigInt.fromI32(0)
-    }
-    newNFTBalance.amount = newNFTBalance.amount + BigInt.fromI32(1)
-    newNFTBalance.save()
+    // let previousOwner = event.params.from.toHex()
+    // let nftBalance = NftBalance.load(previousOwner)
+    // if (nftBalance != null) {
+    //     if (nftBalance.amount > BigInt.fromI32(0)) {
+    //         nftBalance.amount = nftBalance.amount - BigInt.fromI32(1)
+    //     }
+    //     nftBalance.save()
+    // }    
+
+    // let newOwner = event.params.to.toHex()
+    // let newNFTBalance = NftBalance.load(newOwner)
+    // if (newNFTBalance == null) {
+    //     newNFTBalance = new NftBalance(newOwner)
+    //     newNFTBalance.amount = BigInt.fromI32(0)
+    // }
+    // newNFTBalance.amount = newNFTBalance.amount + BigInt.fromI32(1)
+    // newNFTBalance.save()
 }
