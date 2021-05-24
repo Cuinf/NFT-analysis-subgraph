93b7c5de6f8fa26a918d4d2f0692be2bf59346f1
Cui Qin <qincuidhu@gmail.com>
2021-05-24 11:06:21 +0200

    remove nftBalance related

--- src/mapping_cryptokitties.ts
+++ src/mapping_cryptokitties.ts
@@ -10,9 +10,9 @@ export function handleBirth(event: Birth): void {
     kitty.contract = event.address
     kitty.save()
 
-    let kittyBalance = new NftBalance(event.params.owner.toHex())
-    kittyBalance.amount = BigInt.fromI32(1)
-    kittyBalance.save()
+    // let kittyBalance = new NftBalance(event.params.owner.toHex())
+    // kittyBalance.amount = BigInt.fromI32(1)
+    // kittyBalance.save()
 }
 
 export function handleTransfer(event: Transfer): void {
